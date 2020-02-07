615e2198c11a703d6093e5b9dbf8af61e11e3b6c
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2020-02-07 16:41:11 +0100

    Add missing file required for testing on staging environment.
    (cherry picked from commit 442b0bd0a1519abd878f5fb8cbe6dc5252bfbfab)

---
+++ Alpaca.Markets.Tests/Staging.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Alpaca.Markets.Tests
+{
+    internal sealed class Staging : IEnvironment
+    {
+        private Staging() {}
+
+        public static IEnvironment Environment { get; } = new Staging();
+
+        public Uri AlpacaTradingApi { get; } = new Uri("https://staging-api.tradetalk.us");
+
+        public Uri AlpacaDataApi => Environments.Live.AlpacaDataApi;
+
+        public Uri PolygonDataApi => Environments.Live.PolygonDataApi;
+
+        public Uri AlpacaStreamingApi { get; } = new Uri("wss://staging-api.tradetalk.us/stream");
+
+        public Uri PolygonStreamingApi => Environments.Live.PolygonStreamingApi;
+    }
+}
