87db9c3b75793d780cc74b69859ecacd2657c271
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2021-12-28 12:54:56 +0100

    Fix several R# issues in code (new version of tool in VS 2022).

--- Alpaca.Markets.Tests/ClientsFactory.cs
+++ Alpaca.Markets.Tests/ClientsFactory.cs
@@ -4,47 +4,49 @@ using System.IO;
 using Microsoft.Extensions.Configuration;
 using Xunit;
 
-namespace Alpaca.Markets.Tests
+namespace Alpaca.Markets.Tests;
+
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+public sealed class PaperEnvironmentClientsFactoryFixture
 {
-    [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
-    public sealed class PaperEnvironmentClientsFactoryFixture
+    private readonly String _alpacaKeyId;
+
+    private readonly String _alpacaSecretKey;
+
+    public PaperEnvironmentClientsFactoryFixture()
     {
-        private readonly String _alpacaKeyId;
+        var configuration = new ConfigurationBuilder()
+            .AddEnvironmentVariables()
+            .AddJsonFile(Path.Combine(
+                Environment.CurrentDirectory, @"..\..\..\..\Development.json"), true)
+            .Build();
 
-        private readonly String _alpacaSecretKey;
+        _alpacaKeyId = configuration["PAPER_ALPACA_KEY_ID"] ?? String.Empty;
+        _alpacaSecretKey = configuration["PAPER_ALPACA_SECRET_KEY"] ?? String.Empty;
 
-        public PaperEnvironmentClientsFactoryFixture()
+        if (String.IsNullOrEmpty(_alpacaKeyId))
         {
-            var configuration = new ConfigurationBuilder()
-                .AddEnvironmentVariables()
-                .AddJsonFile(Path.Combine(
-                    Environment.CurrentDirectory, @"..\..\..\..\Development.json"), true)
-                .Build();
-
-            _alpacaKeyId = configuration["PAPER_ALPACA_KEY_ID"] ?? String.Empty;
-            _alpacaSecretKey = configuration["PAPER_ALPACA_SECRET_KEY"] ?? String.Empty;
-
-            if (String.IsNullOrEmpty(_alpacaKeyId))
-            {
-                _alpacaKeyId = Guid.NewGuid().ToString("N");
-            }
+            _alpacaKeyId = Guid.NewGuid().ToString("N");
         }
-
-        public IAlpacaDataClient GetAlpacaDataClient() =>
-            Environments.Paper.GetAlpacaDataClient(getSecretKey());
-
-        public IAlpacaTradingClient GetAlpacaTradingClient() =>
-            Environments.Paper.GetAlpacaTradingClient(getSecretKey());
-
-        public IAlpacaStreamingClient GetAlpacaStreamingClient() =>
-            Environments.Paper.GetAlpacaStreamingClient(getSecretKey());
-
-        public IAlpacaDataStreamingClient GetAlpacaDataStreamingClient() =>
-            Environments.Paper.GetAlpacaDataStreamingClient(getSecretKey());
-
-        private SecretKey getSecretKey() => new SecretKey(_alpacaKeyId, _alpacaSecretKey);
     }
 
-    [CollectionDefinition("PaperEnvironment")]
-    public sealed class PaperEnvironmentClientsFactoryCollection : ICollectionFixture<PaperEnvironmentClientsFactoryFixture> { }
+    public IAlpacaDataClient GetAlpacaDataClient() =>
+        Environments.Paper.GetAlpacaDataClient(getSecretKey());
+
+    public IAlpacaTradingClient GetAlpacaTradingClient() =>
+        Environments.Paper.GetAlpacaTradingClient(getSecretKey());
+
+    public IAlpacaStreamingClient GetAlpacaStreamingClient() =>
+        Environments.Paper.GetAlpacaStreamingClient(getSecretKey());
+
+    public IAlpacaDataStreamingClient GetAlpacaDataStreamingClient() =>
+        Environments.Paper.GetAlpacaDataStreamingClient(getSecretKey());
+
+    private SecretKey getSecretKey() => new (_alpacaKeyId, _alpacaSecretKey);
+}
+
+[CollectionDefinition("PaperEnvironment")]
+public sealed class PaperEnvironmentClientsFactoryCollection
+    : ICollectionFixture<PaperEnvironmentClientsFactoryFixture>
+{
 }
